88524840d35815a0bff7a43bcd387a4f4f2e2522
Noam Ben Sason <noam.bensason@gmail.com>
2024-09-17 19:30:24 +0300

    run up to this point

--- dp_chest_xray.py
+++ dp_chest_xray.py
@@ -8,7 +8,9 @@ from opacus import PrivacyEngine
 from torchvision import datasets, transforms
 from torchvision.datasets import ImageFolder
 from tqdm import tqdm
-
+import time
+import json
+import codecs
 import os
 import pandas as pd
 from torchvision.io import read_image
@@ -75,11 +77,14 @@ class DPConvNet(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 100, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(100, 150, kernel_size=3, padding=1)
+
         self.conv3 = nn.Conv2d(150, 200, kernel_size=3, padding=1)
         self.conv4 = nn.Conv2d(200, 200, kernel_size=3, padding=1)
+
         self.conv5 = nn.Conv2d(200, 250, kernel_size=3, padding=1)
         self.conv6 = nn.Conv2d(250, 250, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(342250, 64)
+
+        self.fc1 = nn.Linear(36000, 64)
         self.fc2 = nn.Linear(64, 32)
         self.fc3 = nn.Linear(32, 16)
         self.fc4 = nn.Linear(16, 8)
@@ -126,16 +131,18 @@ def train(args, model, device, train_loader, optimizer, privacy_engine, epoch):
         optimizer.step()
         losses.append(loss.item())
 
+    avg_train_loss = np.mean(losses)
     if not args.disable_dp:
         epsilon = privacy_engine.accountant.get_epsilon(delta=args.delta)
         print(
             f"Train Epoch: {epoch} \t"
-            f"Loss: {np.mean(losses):.6f} "
+            f"Loss: {avg_train_loss:.6f} "
             f"(ε = {epsilon:.2f}, δ = {args.delta}) privacy budget spent so far"
         )
     else:
-        print(f"Train Epoch: {epoch} \t Loss: {np.mean(losses):.6f}")
+        print(f"Train Epoch: {epoch} \t Loss: {avg_train_loss:.6f}")
 
+    return avg_train_loss
 
 def test(model, device, test_loader):
     model.eval()
@@ -162,40 +169,61 @@ def test(model, device, test_loader):
             100.0 * correct / len(test_loader.dataset),
         )
     )
-    return correct / len(test_loader.dataset)
+
+    test_accuracy = correct / len(test_loader.dataset)
+    return test_accuracy, test_loss
 
 
-def get_transforms():
-    train_transform = transforms.Compose([
-        # transforms.RandomRotation(10),  # rotate +/- 10 degrees
-        # transforms.RandomHorizontalFlip(),  # reverse 50% of images
-        transforms.Resize((300, 300)),
-        # transforms.CenterCrop(100),  # crop longest side
-        ToTensor(),
-        # transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])
-    ])
+def get_transforms(config):
+    if not config["BASIC_TRANSFORM"]:
+        train_transform = transforms.Compose([
+            transforms.RandomRotation(10),  # rotate +/- 10 degrees
+            transforms.RandomHorizontalFlip(),  # reverse 50% of images
+            transforms.Resize(100),  # resize shortest side
+            transforms.CenterCrop(100),  # crop longest side
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406],
+                                 [0.229, 0.224, 0.225])
+        ])
+    else:
+        train_transform = transforms.Compose([
+            transforms.Resize((100, 100)),
+            ToTensor(),
+        ])
 
     return train_transform
 
 
 def main():
+    # python dp_chest_xray.py - -device = cuda - n = 10 - -lr = 0.001 - -sigma = 1.3 - c = 1.5 - b = 16
+    print("DP with Chest X-ray")
+
     # Training settings
     parser = argparse.ArgumentParser(
         description="DP with Chest X-ray",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
+
+    parser.add_argument('--config_name', type=str, help='config name')
+
+    args = parser.parse_args()
+    config_name = args.config_name
+
+    with open(f"configs/{config_name}.json") as f:
+        config = json.load(f)
+
     parser.add_argument(
         "-b",
         "--batch-size",
         type=int,
-        default=64,
+        default=config["BATCH_SIZE"],
         metavar="B",
         help="Batch size",
     )
     parser.add_argument(
         "--test-batch-size",
         type=int,
-        default=1024,
+        default=config["TEST_BATCH_SIZE"],
         metavar="TB",
         help="input batch size for testing",
     )
@@ -203,7 +231,7 @@ def main():
         "-n",
         "--epochs",
         type=int,
-        default=10,
+        default=config["NUM_EPOCHS"],
         metavar="N",
         help="number of epochs to train",
     )
@@ -211,21 +239,21 @@ def main():
         "-r",
         "--n-runs",
         type=int,
-        default=1,
+        default=config["NUM_RUNS"],
         metavar="R",
         help="number of runs to average on",
     )
     parser.add_argument(
         "--lr",
         type=float,
-        default=0.1,
+        default=config["LR"],
         metavar="LR",
         help="learning rate",
     )
     parser.add_argument(
         "--sigma",
         type=float,
-        default=1.0,
+        default=config["SIGMA"],
         metavar="S",
         help="Noise multiplier",
     )
@@ -233,54 +261,66 @@ def main():
         "-c",
         "--max-per-sample-grad_norm",
         type=float,
-        default=1.0,
+        default=config["C"],
         metavar="C",
         help="Clip per-sample gradients to this norm",
     )
     parser.add_argument(
         "--delta",
         type=float,
-        default=1e-5,
+        default=config["DELTA"],
         metavar="D",
         help="Target delta",
     )
     parser.add_argument(
         "--device",
         type=str,
-        default="cuda",
+        default=config["DEVICE"],
         help="GPU ID for this process",
     )
     parser.add_argument(
         "--save-model",
         action="store_true",
-        default=False,
+        default=config["SAVE_MODEL"],
         help="Save the trained model",
     )
     parser.add_argument(
         "--disable-dp",
         action="store_true",
-        default=False,
+        default=config["DISABLE_DP"],
         help="Disable privacy training and just train with vanilla SGD",
     )
     parser.add_argument(
         "--secure-rng",
         action="store_true",
-        default=False,
+        default=config["SECURE_RNG"],
         help="Enable Secure RNG to have trustworthy privacy guarantees. Comes at a performance cost",
     )
+
+    parser.add_argument(
+        '--optimizer',
+        type=str,
+        default=config["OPTIMIZER"],
+        help='optimizer name')
+
+    parser.add_argument(
+        '--basic_transform',
+        type=str,
+        default=config["BASIC_TRANSFORM"],
+        help='If to do basic transform or fancy transform')
 
     args = parser.parse_args()
     device = torch.device(args.device)
 
     train_loader = torch.utils.data.DataLoader(
-        ImageFolder(DATA_DIR + "train", transform=get_transforms()),
+        ImageFolder(DATA_DIR + "train", transform=get_transforms(config)),
         batch_size=args.batch_size,
         num_workers=0,
         pin_memory=True,
 
     )
     test_loader = torch.utils.data.DataLoader(
-        ImageFolder(DATA_DIR + "test", transform=get_transforms()),
+        ImageFolder(DATA_DIR + "test", transform=get_transforms(config)),
         batch_size=args.test_batch_size,
         shuffle=True,
         num_workers=0,
@@ -288,15 +328,24 @@ def main():
 
     )
 
-    # example, _ = ImageFolder(DATA_DIR+"train", transform=get_transforms())[20]
+    # example, _ = ImageFolder(DATA_DIR+"train", transform=get_transforms(config))[20]
     # plt.imshow(example.permute(1, 2, 0))
     # plt.show()
 
-    run_results = []
-    for _ in range(args.n_runs):
+    run_results_train_accuracy = []
+    run_results_test_accuracy = []
+    run_results_train_loss = []
+    run_results_test_loss = []
+
+    train_loss_over_epochs = np.zeros((args.n_runs,args.epochs))
+
+    for run_i in range(args.n_runs): # number of experiments
         model = DPConvNet().to(device)
 
-        optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0)
+        if args.optimizer == 'adam':
+            optimizer = optim.Adam(model.parameters(), lr=args.lr)
+        else: # args.optimizer == 'sgd':
+            optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0)
         privacy_engine = None
 
         if not args.disable_dp:
@@ -306,28 +355,61 @@ def main():
                 optimizer=optimizer,
                 data_loader=train_loader,
                 noise_multiplier=args.sigma,
-                max_grad_norm=args.max_per_sample_grad_norm,
+                max_grad_norm=args.max_per_sample_grad_norm, # C
             )
-
         for epoch in range(1, args.epochs + 1):
-            train(args, model, device, train_loader, optimizer, privacy_engine, epoch)
-        run_results.append(test(model, device, test_loader))
+            train_loss_mid_train = train(args, model, device, train_loader, optimizer, privacy_engine, epoch)
+            train_loss_over_epochs[run_i][epoch-1] = train_loss_mid_train
 
-    if len(run_results) > 1:
+        train_accuracy, train_loss = test(model, device, train_loader)
+        test_accuracy, test_loss = test(model, device, test_loader)
+
+        run_results_train_accuracy.append(train_accuracy)
+        run_results_train_loss.append(train_loss)
+
+        run_results_test_accuracy.append(test_accuracy)
+        run_results_test_loss.append(test_loss)
+
+    if len(run_results_test_accuracy) > 1:
         print(
             "Accuracy averaged over {} runs: {:.2f}% ± {:.2f}%".format(
-                len(run_results), np.mean(run_results) * 100, np.std(run_results) * 100
+                len(run_results_test_accuracy), np.mean(run_results_test_accuracy) * 100, np.std(run_results_test_accuracy) * 100
             )
         )
+    os.makedirs("out", exist_ok=True)
+    os.makedirs("out/results", exist_ok=True)
+    os.makedirs("out/saved_models", exist_ok=True)
+    time_str = time.strftime("%Y%m%d-%H%M%S")
 
     repro_str = (
         f"chest_xray_{args.lr}_{args.sigma}_"
         f"{args.max_per_sample_grad_norm}_{args.batch_size}_{args.epochs}"
     )
-    torch.save(run_results, f"run_results_{repro_str}.pt")
+    torch.save(run_results_test_accuracy, f"out/results/run_results_{config_name}_{time_str}.pt")
+
+    with open(f"out/results/final_results_{config_name}_{time_str}.json", "w") as f:
+        json.dump({
+            "config_name": config_name,
+            "config": config,
+            "avg_train_loss": np.mean(run_results_train_loss),
+            "avg_train_accuracy": np.mean(run_results_train_accuracy),
+            "std_train_accuracy": np.std(run_results_train_accuracy),
+
+            "avg_test_loss": np.mean(run_results_test_loss),
+            "avg_test_accuracy": np.mean(run_results_test_accuracy),
+            "std_test_accuracy": np.std(run_results_test_accuracy),
+
+        },f)
+
+    train_loss_over_epochs = train_loss_over_epochs.tolist()
+    file_path = f"out/results/train_loss_over_time_{config_name}_{time_str}.json"
+    json.dump(train_loss_over_epochs, codecs.open(file_path, 'w', encoding='utf-8'),
+              separators=(',', ':'),
+              sort_keys=True,
+              indent=4)
 
     if args.save_model:
-        torch.save(model.state_dict(), f"chest_xray_cnn_{repro_str}.pt")
+        torch.save(model.state_dict(), f"out/saved_models/chest_xray_cnn_{config_name}_{time_str}.pt")
 
 
 if __name__ == "__main__":
--- run_dp_chest_xray.sh
+++ run_dp_chest_xray.sh
@@ -1,15 +1,18 @@
 #!/bin/csh
 #SBATCH --time=5:00:00
 #SBATCH --mem=128GB
-#SBATCH --output=privacy_mnist_example_%A.out
-#SBATCH --job-name=privacy_mnist_example_%A
+#SBATCH -c4
+#SBATCH --output=privacy_dp_chest_xray_%A.out
+#SBATCH --job-name=privacy_dp_chest_xray_%A
 #SBATCH --mail-type=END,FAIL,BEGIN
 #SBATCH --mail-user=noam.bensason@mail.huji.ac.il
-#SBATCH --gres=gpu:1,vmem:32G
+#SBATCH --gres=gpu:1,vmem:24G
+#SBATCH --killable
+
 
 cd /cs/labs/tomhope/noam_bs97/advanced_privacy_project
 module load cuda/11.8
 source /cs/labs/tomhope/noam_bs97/miniconda3/etc/profile.d/conda.csh
 conda activate privacy_env
 
-python dp_chest_xray.py --device=cuda -n=10 --lr=0.001 --sigma=1.3 -c=1.5 -b=16
+python dp_chest_xray.py --config_name "config6"
